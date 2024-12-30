68a3151a0c9f8ae602d5d48d21fa5fedbedfe0b2
shivam kumar  singh <113622685+Shivam7235singh@users.noreply.github.com>
2024-12-30 09:55:59 +0530

    shivam singh

--- config/db.js
+++ config/db.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-const  db = mysql.createPool({
-    host : 'localhost',
-    user : 'root',
-    password : 'Shivam@041',
-    database : "student_db",
-})
+
+const db = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
 
 
 
